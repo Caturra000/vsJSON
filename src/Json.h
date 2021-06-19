a07b812c45e5b393488c4e1426a53bfb1a701daa
Caturra <wasdjkl233@gmail.com>
2021-06-20 01:58:43 +0800

    make interface clean and simple

--- src/Json.h
+++ src/Json.h
@@ -7,166 +7,176 @@
 namespace vsjson {
 
 class Json {
+public: // static
+    template <typename ...Args>
+    static JsonValue array(Args &&...elems);
+
 public:
-
     Json(): _value(ObjectImpl()) {}
-
     template <typename ...Args>
     Json(Args &&...args): _value(std::forward<Args>(args)...) {}
-
     template <size_t N>
     Json(const char (&str)[N]): _value(StringImpl(str)) {}
-
     Json(bool b): _value(BooleanImpl(b)) {}
     Json(nullptr_t): _value(NullImpl(nullptr)) {}
     Json(const std::string &str): _value(StringImpl(str)) {}
     Json(std::string &&str): _value(StringImpl(static_cast<std::string&&>(str))) {}
+    Json(const std::initializer_list<ObjectImpl::value_type> &list): _value(ObjectImpl(list)) {}
+    Json(std::initializer_list<ObjectImpl::value_type> &&list): _value(ObjectImpl(std::move(list))) {}
 
     Json(const Json &rhs): _value(rhs._value) {}
     Json(Json &&rhs): _value(std::move(rhs._value)) {}
 
-    Json(const std::initializer_list<ObjectImpl::value_type> &list)
-        : _value(ObjectImpl(list)) {}
+    template <typename T,
+              typename = std::enable_if_t<!std::is_same<std::decay_t<T>, Json>::value>>
+    Json& operator=(T&& obj);
+    Json& operator=(const Json &rhs);
+    Json& operator=(Json &&rhs);
+    template <size_t N>
+    Json& operator=(const char (&str)[N]);
+    Json& operator=(bool b);
+    Json& operator=(nullptr_t);
+    Json& operator=(const std::string &str);
+    Json& operator=(std::string &&str);
 
-    Json(std::initializer_list<ObjectImpl::value_type> &&list)
-        : _value(ObjectImpl(std::move(list))) {}
+    template <typename T> bool is() const { return _value.is<T>(); }
+    template <typename T> T& as() { return _value.get<T>(); }
+    template <typename T> T to() const & { return _value.to<T>(); }
+    template <typename T> T to() && { return std::move(_value).to<T>(); }
 
+    bool contains(const StringImpl &index) const;
+
+    Json& operator[](const StringImpl &index);
+    Json& operator[](size_t index);
+    const Json& operator[](size_t index) const;
+
+    size_t size() const;
+    size_t arraySize() const;
+
+    void append(const Json &json);
+    void append(Json &&json);
+
+    auto begin() -> ObjectImpl::iterator;
+    auto end() -> ObjectImpl::iterator;
+
+    std::string dump();
+    template <typename ...Specifieds>
+    std::string dump(Specifieds &&...ses);
 
     // FIXME: use swap on operator=
-    void swap(Json &rhs) {
-        std::swap(*this, rhs);
-    }
+    void swap(Json &rhs) { std::swap(*this, rhs); }
 
-    template <typename T,
-    typename = std::enable_if_t<!std::is_same<std::decay_t<T>, Json>::value>>
-    Json& operator=(T&& obj) {
-        this->~Json();
-        _value = std::forward<T>(obj);
-        return *this;
-    }
-
-    Json& operator=(const Json &rhs) {
-        if(this == &rhs) return *this;
-        this->~Json();
-        _value = rhs._value;
-        return *this;
-    }
-
-    Json& operator=(Json &&rhs) {
-        if(this == &rhs) return *this;
-        this->~Json();
-        _value = std::move(rhs._value);
-        return *this;
-    }
-
-    template <size_t N>
-    Json& operator=(const char (&str)[N]) {
-        _value = StringImpl(str);
-        return *this;
-    }
-
-    Json& operator=(bool b) {
-        _value = BooleanImpl(b);
-        return *this;
-    }
-
-    Json& operator=(nullptr_t) {
-        _value = NullImpl(nullptr);
-        return *this;
-    }
-
-    Json& operator=(const std::string &str) {
-        _value = StringImpl(str);
-        return *this;
-    }
-
-    Json& operator=(std::string &&str) {
-        _value = StringImpl(static_cast<std::string&&>(str));
-        return *this;
-    }
-
-    JsonValue& value() { return _value; }
-
-    template <typename ...Args>
-    static JsonValue array(Args &&...elems) {
-        return JsonValue(ArrayImpl{std::forward<Args>(elems)...});
-    }
-
-    friend std::ostream& operator<<(std::ostream &os, Json &json) {
-        os << json.value();
-        return os;
-    }
-
-    template <typename T>
-    bool is() const { return _value.is<T>(); }
-
-    template <typename T>
-    T& as()  { return _value.get<T>(); }
-
-    template <typename T>
-    T to() const & { return _value.to<T>(); }
-
-    template <typename T>
-    T to() && { return std::move(_value).to<T>(); }
-
-    bool contains(const StringImpl &index) const {
-        return _value.get<ObjectImpl>().count(index);
-    }
-
-    Json& operator[](const StringImpl &index) {
-        return _value.get<ObjectImpl>()[index];
-    }
-
-    Json& operator[](size_t index) {
-        return _value.get<ArrayImpl>()[index];
-    }
-
-    const Json& operator[](size_t index) const {
-        return _value.get<ArrayImpl>()[index];
-    }
-
-    size_t size() const {
-        return _value.get<ObjectImpl>().size();
-    }
-
-    size_t arraySize() const {
-        return _value.get<ArrayImpl>().size();
-    }
-
-    void append(const Json &json) {
-        _value.get<ArrayImpl>().emplace_back(json);
-    }
-
-    void append(Json &&json) {
-        _value.get<ArrayImpl>().emplace_back(static_cast<Json&&>(json));
-    }
-
-    std::string dump() {
-        std::stringstream ss;
-        ss << (*this);
-        return ss.str();
-    }
-
-    template <typename ...Specifieds>
-    std::string dump(Specifieds &&...ses) {
-        std::stringstream ss;
-        // set stream status or put data before dump
-        std::initializer_list<int> {((ss << std::forward<Specifieds>(ses)), 19260817)...};
-        ss << (*this);
-        return ss.str();
-    }
-
-    auto begin() -> ObjectImpl::iterator {
-        return _value.get<ObjectImpl>().begin();
-    }
-
-    auto end() -> ObjectImpl::iterator {
-        return _value.get<ObjectImpl>().end();
-    }
+    friend std::ostream& operator<<(std::ostream &os, vsjson::Json &json) { return os << json._value; }
 
 private:
     JsonValue _value;
 };
+
+template <typename ...Args>
+inline JsonValue Json::array(Args &&...elems) {
+    return JsonValue(ArrayImpl{std::forward<Args>(elems)...});
+}
+
+template <typename T,typename>
+inline Json& Json::operator=(T&& obj) {
+    this->~Json();
+    _value = std::forward<T>(obj);
+    return *this;
+}
+
+inline Json& Json::operator=(const Json &rhs) {
+    if(this == &rhs) return *this;
+    this->~Json();
+    _value = rhs._value;
+    return *this;
+}
+
+inline Json& Json::operator=(Json &&rhs) {
+    if(this == &rhs) return *this;
+    this->~Json();
+    _value = std::move(rhs._value);
+    return *this;
+}
+
+template <size_t N>
+inline Json& Json::operator=(const char (&str)[N]) {
+    _value = StringImpl(str);
+    return *this;
+}
+
+inline Json& Json::operator=(bool b) {
+    _value = BooleanImpl(b);
+    return *this;
+}
+
+inline Json& Json::operator=(nullptr_t) {
+    _value = NullImpl(nullptr);
+    return *this;
+}
+
+inline Json& Json::operator=(const std::string &str) {
+    _value = StringImpl(str);
+    return *this;
+}
+
+inline Json& Json::operator=(std::string &&str) {
+    _value = StringImpl(static_cast<std::string&&>(str));
+    return *this;
+}
+
+inline bool Json::contains(const StringImpl &index) const {
+    return _value.get<ObjectImpl>().count(index);
+}
+
+inline Json& Json::operator[](const StringImpl &index) {
+    return _value.get<ObjectImpl>()[index];
+}
+inline Json& Json::operator[](size_t index) {
+    return _value.get<ArrayImpl>()[index];
+}
+inline const Json& Json::operator[](size_t index) const {
+    return _value.get<ArrayImpl>()[index];
+}
+
+inline size_t Json::size() const {
+    return _value.get<ObjectImpl>().size();
+}
+
+inline size_t Json::arraySize() const {
+    return _value.get<ArrayImpl>().size();
+}
+
+inline void Json::append(const Json &json) {
+    _value.get<ArrayImpl>().emplace_back(json);
+}
+inline void Json::append(Json &&json) {
+    _value.get<ArrayImpl>().emplace_back(static_cast<Json&&>(json));
+}
+
+inline auto Json::begin() -> ObjectImpl::iterator {
+    return _value.get<ObjectImpl>().begin();
+}
+
+inline auto Json::end() -> ObjectImpl::iterator {
+    return _value.get<ObjectImpl>().end();
+}
+
+inline std::string Json::dump() {
+    std::stringstream ss;
+    ss << (*this);
+    return ss.str();
+}
+
+template <typename ...Specifieds>
+inline std::string Json::dump(Specifieds &&...ses) {
+    std::stringstream ss;
+    // set stream status or put data before dump
+    std::initializer_list<int> {((ss << std::forward<Specifieds>(ses)), 19260817)...};
+    ss << (*this);
+    return ss.str();
+}
+
+/// specialization
 
 template <>
 inline bool Json::is<bool>() const { return _value.is<BooleanImpl>(); }
--- src/internal/Variant.h
+++ src/internal/Variant.h
@@ -15,130 +15,46 @@ struct RuntimeChoose {
 
 template <typename ...Ts>
 class Variant {
-private:
-    int _what;
-    char _handle[MaxSize<Ts...>::size];
-
 public:
-    Variant() {
-        _what = -1;
-        memset(_handle, 0, sizeof _handle);
-    }
+    Variant();
+    template <typename T,
+              typename = RequireNot<std::is_same<std::decay_t<T>, Variant>>>
+    Variant(T &&obj);
+    Variant(const Variant &rhs);
+    Variant(Variant &&rhs);
 
     template <typename T,
-    typename = RequireNot<std::is_same<std::decay_t<T>, Variant>>>
-    Variant(T &&obj) {
-        static_assert(Position<std::decay_t<T>, Ts...>::pos != -1,
-            "type not found");
-        init(std::forward<T>(obj));
-    }
+              typename = RequireNot<std::is_same<std::decay_t<T>, Variant>>>
+    Variant& operator=(T &&rhs);
+    Variant& operator=(const Variant &rhs);
+    Variant& operator=(Variant &&rhs);
 
-    Variant(const Variant &rhs) {
-        visitor::CopyConstructVisitor<Ts...> ccv(*this);
-        const_cast<Variant&>(rhs).visit(ccv);
-    }
+    ~Variant();
 
-    Variant(Variant &&rhs) {
-        visitor::MoveConstructVisitor<Ts...> mcv(*this);
-        rhs.visit(mcv);
-        // rhs._what = -1;
-        // memset(rhs._handle, 0, sizeof(rhs._handle));
-    }
+    template <typename T> T& get();
+    template <typename T> const T& get() const;
 
-    // DO NOT IMPLEMENT operator= BY SWAP(swap for placement new buffer is wrong)
-    // reverse case : implements swap by operator=&&
-    void swap(Variant &rhs) {
-       std::swap(*this, rhs);
-    }
+    template <typename T> bool is() const;
 
-    template <typename T,
-    typename = RequireNot<std::is_same<std::decay_t<T>, Variant>>>
-    Variant& operator=(T &&rhs) {
-        this->~Variant();
-        init(std::forward<T>(rhs));
-        return *this;
-    }
+    template <typename T> T to() &;
+    template <typename T> T to() const &;
+    template <typename T> T to() &&;
 
-    Variant& operator=(const Variant &rhs){
-        if(this == &rhs) return *this;
-        this->~Variant();
-        visitor::CopyConstructVisitor<Ts...> ccv(*this);
-        const_cast<Variant&>(rhs).visit(ccv);
-        return *this;
-    }
+    template <typename Visitor,
+              typename R = typename Visitor::ReturnType>
+    R visit(Visitor &visitor);
 
-    Variant& operator=(Variant &&rhs) {
-        if(this == &rhs) return *this;
-        this->~Variant();
-        visitor::MoveConstructVisitor<Ts...> mcv(*this);
-        rhs.visit(mcv);
-        return *this;
-    }
+    void swap(Variant &rhs);
 
-    ~Variant() {
-        if(_what == -1) return;
-        visitor::DeleteVisitor<Ts...> dv(*this);
-        visit(dv);
-    }
-
-    template <typename T>
-    T& get() {
-        static_assert(Position<T, Ts...>::pos != -1,
-            "type not found");
-        if(_what == Position<T, Ts...>::pos) {
-            return *reinterpret_cast<T*>(_handle);
-        } else {
-            throw std::runtime_error("cannot cast");
-        }
-    }
-
-    template <typename T>
-    const T& get() const {
-        return const_cast<Variant*>(this)->get<T>();
-    }
-
-    template <typename T>
-    bool is() const {
-        return Position<T, Ts...>::pos == _what;
-    }
-
-    template <typename T>
-    T to() & {
-        visitor::ConvertVisitor<T> cv;
-        return visit(cv);
-    }
-
-    template <typename T>
-    T to() const & {
-        return const_cast<Variant*>(this)->to<T>();
-    }
-
-    template <typename T>
-    T to() && {
-        visitor::MovedConvertVisitor<T> cv;
-        return visit(cv);
-    }
-
-    template <typename Visitor, typename R = typename Visitor::ReturnType>
-    R visit(Visitor &visitor) {
-        return RuntimeChoose<0, sizeof...(Ts)-1, Ts...>
-            ::apply(_what, _handle, visitor);
-    }
-
-    friend std::ostream& operator<<(std::ostream &os, Variant &variant) {
-        visitor::OsVisitor osv(os);
-        return variant.visit(osv);
-    }
+    template<typename T>
+    void init(T &&obj);
 
     int what() const { return _what; }
     const char* handle() { return _handle; }
 
-    template<typename T>
-    void init(T &&obj) {
-        using DecayT = std::decay_t<T>;
-        _what = Position<DecayT, Ts...>::pos;
-        new(_handle) DecayT(std::forward<T>(obj));
-    }
+private:
+    int _what;
+    char _handle[MaxSize<Ts...>::size];
 };
 
 template <int Lo, int Hi, typename ...Ts>
@@ -153,6 +69,137 @@ inline typename Visitor::ReturnType RuntimeChoose<Lo, Hi, Ts...>::apply(int n, v
     } else {
         return RuntimeChoose<std::min(Mid + 1, Hi), Hi, Ts...>::apply(n, data, visitor);
     }
+}
+
+template <typename ...Ts>
+inline Variant<Ts...>::Variant() {
+    _what = -1;
+    memset(_handle, 0, sizeof _handle);
+}
+
+template <typename ...Ts>
+template <typename T, typename>
+inline Variant<Ts...>::Variant(T &&obj) {
+    static_assert(Position<std::decay_t<T>, Ts...>::pos != -1,
+        "type not found");
+    init(std::forward<T>(obj));
+}
+
+template <typename ...Ts>
+inline Variant<Ts...>::Variant(const Variant &rhs) {
+    visitor::CopyConstructVisitor<Ts...> ccv(*this);
+    const_cast<Variant&>(rhs).visit(ccv);
+}
+
+template <typename ...Ts>
+inline Variant<Ts...>::Variant(Variant &&rhs) {
+    visitor::MoveConstructVisitor<Ts...> mcv(*this);
+    rhs.visit(mcv);
+}
+
+template <typename ...Ts>
+template <typename T,typename>
+inline Variant<Ts...>& Variant<Ts...>::operator=(T &&rhs) {
+    this->~Variant();
+    init(std::forward<T>(rhs));
+    return *this;
+}
+
+template <typename ...Ts>
+inline Variant<Ts...>& Variant<Ts...>::operator=(const Variant &rhs){
+    if(this == &rhs) return *this;
+    this->~Variant();
+    visitor::CopyConstructVisitor<Ts...> ccv(*this);
+    const_cast<Variant&>(rhs).visit(ccv);
+    return *this;
+}
+
+template <typename ...Ts>
+inline Variant<Ts...>& Variant<Ts...>::operator=(Variant &&rhs) {
+    if(this == &rhs) return *this;
+    this->~Variant();
+    visitor::MoveConstructVisitor<Ts...> mcv(*this);
+    rhs.visit(mcv);
+    return *this;
+}
+
+template <typename ...Ts>
+inline Variant<Ts...>::~Variant() {
+    if(_what == -1) return;
+    visitor::DeleteVisitor<Ts...> dv(*this);
+    visit(dv);
+}
+
+template <typename ...Ts>
+template <typename T>
+inline T& Variant<Ts...>::get() {
+    static_assert(Position<T, Ts...>::pos != -1,
+        "type not found");
+    if(_what == Position<T, Ts...>::pos) {
+        return *reinterpret_cast<T*>(_handle);
+    } else {
+        throw std::runtime_error("cannot cast");
+    }
+}
+
+template <typename ...Ts>
+template <typename T>
+inline const T& Variant<Ts...>::get() const {
+    return const_cast<Variant*>(this)->get<T>();
+}
+
+template <typename ...Ts>
+template <typename T>
+inline bool Variant<Ts...>::is() const {
+    return Position<T, Ts...>::pos == _what;
+}
+
+template <typename ...Ts>
+template <typename T>
+inline T Variant<Ts...>::to() & {
+    visitor::ConvertVisitor<T> cv;
+    return visit(cv);
+}
+
+template <typename ...Ts>
+template <typename T>
+inline T Variant<Ts...>::to() const & {
+    return const_cast<Variant*>(this)->to<T>();
+}
+
+template <typename ...Ts>
+template <typename T>
+inline T Variant<Ts...>::to() && {
+    visitor::MovedConvertVisitor<T> cv;
+    return visit(cv);
+}
+
+template <typename ...Ts>
+template <typename Visitor, typename R>
+inline R Variant<Ts...>::visit(Visitor &visitor) {
+    return RuntimeChoose<0, sizeof...(Ts)-1, Ts...>
+        ::apply(_what, _handle, visitor);
+}
+
+// DO NOT IMPLEMENT operator= BY SWAP(swap for placement new buffer is wrong)
+// reverse case : implements swap by operator=&&
+template <typename ...Ts>
+void Variant<Ts...>::swap(Variant &rhs) {
+    std::swap(*this, rhs);
+}
+
+template <typename ...Ts>
+template<typename T>
+inline void Variant<Ts...>::init(T &&obj) {
+    using DecayT = std::decay_t<T>;
+    _what = Position<DecayT, Ts...>::pos;
+    new(_handle) DecayT(std::forward<T>(obj));
+}
+
+template <typename ...Ts>
+inline std::ostream& operator<<(std::ostream &os, Variant<Ts...> &variant) {
+    visitor::OsVisitor osv(os);
+    return variant.visit(osv);
 }
 
 } // detail
